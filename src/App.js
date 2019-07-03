f47bc4751e88085e19280a0a56ad10b882169d2a
Myles <Mfrazier056@gmail.com>
2019-07-03 18:03:50 -0400

    mf

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
-import Popular from './Popular';
+import Popular from './popular';
 import axios from 'axios';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor(props)
   {
     super(props)
-
+    const show = "";
     this.state = {
       movies: []
-    }
+    } 
 
     // hey, go grab all of the stuff from that webpage
     axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b6fbc7f3f313bd395902af464ef47262")
@@ -21,30 +22,35 @@ class App extends React.Component {
     console.log('right after that axios get stuff!')
     // then put it into this.state.movies
   }
-
+  getPopular(){
+    this.state.movies.map((movie) => {
+      return (
+        <div>
+        <Popular
+        title={movie.title}
+        rating={movie.vote_average}
+        poster={movie.poster_path}
+        desc={movie.overview}
+        />
+        </div>
+      )
+    }
+    )
+  
+  }
   render() {
     console.log('Render function!', this.state.movies);
+    var test = this.getPopular();
     return (
-      <div className="App">
-        {this.state.movies.map((movie) => {
-          return (
-            <div>
-            <Popular
-            title={movie.title}
-            rating={movie.vote_average}
-            poster={movie.poster_path}
-            desc={movie.overview}
-            />
-            </div>
-          )
-        }
-        )
-      }
-        </div>
+      
+      <div className="App" value ={test}>
+        <h1>{test}</h1>
+        swad
+      </div>
     )
   }
 }
-
+//ReactDOM.render(<App/>, document.getElementById('app'))
         
 
         //   <div>
--- src/popular.js
+++ src/popular.js
@@ -6,10 +6,9 @@ class Popular extends Component {
     render() {
         return (
             <div>
-                <h2> {this.props.title}</h2>
+                <h2>{this.props.title}</h2>
                 <img src={'https://image.tmdb.org/t/p/w500' + this.props.poster} alt="Movie Poster" />
                 <p>{this.props.desc}</p>
-
             </div>
         )
     }
